451fe1322054a13d61ef1c7cda438866eb72d4fb
Frank <zhao.yi2005@icloud.com>
2026-08-04 19:43:48 +0800

    Expose recovered QwenIME wire codec

--- src/neural_weasel/qwenime_compat/__init__.py
+++ src/neural_weasel/qwenime_compat/__init__.py
@@ -18,6 +18,16 @@ from .swap_plan import (
     ServerSwapPlan,
     build_server_swap_plan,
 )
+from .wire import (
+    QwenImeWireError,
+    WirePacket,
+    decode_wire_packet,
+    encode_wire_packet,
+    sanitize_windows_username,
+    windows_control_pipe_name,
+    windows_ipc_pipe_name,
+    windows_utility_pipe_name,
+)
 
 __all__ = [
     "CandidateView",
@@ -27,12 +37,20 @@ __all__ = [
     "QwenImeBridge",
     "QwenImeProtocolError",
     "QwenImeSwapPlanError",
+    "QwenImeWireError",
     "RequestKind",
     "SUPPORTED_QWENIME_VERSION",
     "ServerSwapPlan",
+    "WirePacket",
     "build_server_swap_plan",
+    "decode_wire_packet",
+    "encode_wire_packet",
     "parse_json_payload",
     "parse_normalized_request",
+    "sanitize_windows_username",
     "serialize_json_payload",
     "verify_vendor_install",
+    "windows_control_pipe_name",
+    "windows_ipc_pipe_name",
+    "windows_utility_pipe_name",
 ]
